5623b9de6246a0fd0abf87bbcb1f45e915a853f8
Daniel <anuszczyk.daniel@gmail.com>
2018-03-17 21:07:54 +0100

    save reservation data to db

--- WebApplication/WebApplication/Controllers/EndpointController.cs
+++ WebApplication/WebApplication/Controllers/EndpointController.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using Newtonsoft.Json;
 using WebApplication.Models;
+using WebApplication.Repositories;
 
 namespace WebApplication.Controllers
 {
@@ -14,23 +15,35 @@ namespace WebApplication.Controllers
         [HttpPost]
         public ActionResult TestEndpoint()
         {
+            string responseStatus;
+            var responseMessage = string.Empty;
+
             var response = new ResponseModel();
             var transactionModel = EndpointHelper.GetModelFromRequest(Request.InputStream);
+            
+            // TODO: input string log
 
             if (EndpointHelper.CheckTransactionModelHash(transactionModel))
             {
-                
+                var repository = new TestRepository();
+
+                var result = repository.SaveReservation(transactionModel.Reservation);
+
+                responseStatus = result ? "SUCCESS" : "FAILED";
             }
             else
             {
-                response.TransactionId = transactionModel.TransactionId;
-                response.ResponseStatus = "FAILED";
-                response.ResponseMessage = "invalid hash";
+                responseStatus = "FAILED";
+                responseMessage = "invalid hash";
             }
+
+            response.TransactionId = transactionModel.TransactionId;
+            response.ResponseStatus = responseStatus;
+            response.ResponseMessage = responseMessage;
             
+            // TODO: response log
+
             return Content(JsonConvert.SerializeObject(response));
         }
-
-        
     }
 }
---
+++ WebApplication/WebApplication/Repositories/TestRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Web;
+using WebApplication.Models;
+
+namespace WebApplication.Repositories
+{
+    public class TestRepository
+    {
+        private readonly TestDatabaseEntities _databaseEntities = new TestDatabaseEntities();
+
+        public bool SaveReservation(ReservationModel reservationModel)
+        {
+            try
+            {
+                var reservationStatus = new ReservationsStatus
+                {
+                    ReservationId = reservationModel.ReservationId,
+                    Confirmed = reservationModel.ReservationStatus == "CONFIRMED"
+                };
+
+                _databaseEntities.ReservationsStatus.Add(reservationStatus);
+                _databaseEntities.SaveChanges();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                // TODO: add an exception log
+                return false;
+            }
+        }
+    }
+}
